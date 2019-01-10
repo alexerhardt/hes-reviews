00761b1fd9e6eb915e608cd65310e1cc7f34bc12
alexerhardt <alex.erhardt.es@gmail.com>
2019-01-10 10:09:26 +0100

    change webpack config -- now broken

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,48 @@
 const path = require('path');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/app.js',
-    output: {
+module.exports = (env) =>
+{
+  const isProduction = env === 'production';
+
+  return {
+      mode: 'development',
+      entry: './src/app.js',
+      output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
+      },
+      plugins: [
+        new MiniCssExtractPlugin({
+          filename: "styles.css"
+        })
+      ],
+      module: {
+        rules: [
+          {
             test: /\.js$/,
+            loader: 'babel-loader',
             exclude: /node_modules/
-        }, {
+          }, 
+          {
             test: /\.s?css$/,
             use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
+              isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+              {
+                loader: 'css-loader',
+                options: {sourceMap: true}
+              },
+              {
+                loader: 'sass-loader',
+                options: {sourceMap: true}
+              }
             ]
-        }]
-    },
-    devtool: 'cheap-module-source-map',
-    devServer: {
+          }
+        ]
+      },
+      devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+      devServer: {
         contentBase: path.join(__dirname, 'public'),
         historyApiFallback: true
-    }
-};
+      }
+    } 
+}
