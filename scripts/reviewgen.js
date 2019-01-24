b3a9dc3164b2dcb5fd22159952bbfdd3f6a4c9e0
alexerhardt <alex.erhardt.es@gmail.com>
2019-01-24 20:51:37 +0100

    fix script for mock review generation

--- scripts/reviewgen.js
+++ scripts/reviewgen.js
@@ -1,14 +1,11 @@
-const numberOfReviews = 315;
-const reviewText = 
-`
-This is my 2nd class in the program and I really enjoyed it overall. Although I took it in a full semester, I think this would be better as a summer course because there were about 4 "down" weeks in the course where I didn't do anything.
+const mongoose = require('mongoose');
+const faker = require('faker');
 
-The projects span a wide range of topics, but are very manageable. I thought the 1st project, Buffer Overflow was the hardest if you don't have background in C and memory architecture. The 2nd, Malware Analysis, was very straightforward. The Cryptography project was also fairly easy since you don't have to write your own mathematical algorithms unless you really want to. (So you can make it more challenging if you desire by not using libraries.) Finally, 4th Web Security project definitely seemed very hard for those who had never done any web development. If you have encountered JS/HTML/PHP, it is fine.
+const uri = 'mongodb://localhost:27017/hes-dev';
 
-The Canvas quizzes are worth 20% of your grade and open book. So basically, 20% of your grade should be 100% right off the bat. The exams were a bit more random/tricky with some material included that is barely even covered in 2 sentences in the book. I read most of the book, but overall I probably won't retain much since the book is dry and topics are quickly covered. I somewhat regret spending time reading since I don't feel I walked away from the reading with a really deep understanding, but rather with a surface level acquaintance with terminology to pass an exam. I'm sure I will forget all the specialized terminology within a few weeks, hopefully the general concepts stick though.
+const Review = require('../server/models/Review');
+const Course = require('../server/models/Course');
 
-Overall, it was a fun course.
-`;
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -27,29 +24,72 @@ function getRandomDate() {
 }
 
 function getRandomSemester() {
-  const semesters = ["Spring", "Summer", "Fall"];
-  const years = ["2016", "2017", "2018"];
-
+  const semesters = ["january", "spring", "summer", "fall"];
   let s = semesters[getRandomInt(0, semesters.length - 1)];
+  return s;
+}
+
+function getRandomYear() {
+  const years = [2016, 2017, 2018];
   let y = years[getRandomInt(0, years.length - 1)];
-
-  return s + " " + y;
+  return y;
 }
 
 
-const data = [];
+function randomizeReviews(courseId) {
+  const num = getRandomInt(0, 50);
 
-for(let i = 0; i < numberOfReviews; i++) {
-  data.push({
-    author: "fakereviewer@harvard.edu",
-    reviewDate: getRandomDate(),
-    semester: getRandomSemester(),
-    difficulty: getRandomInt(1, 5),
-    courseRating: getRandomInt(1, 5),
-    staffRating: getRandomInt(1, 5),
-    workload: getRandomInt(5, 20),
-    reviewText: reviewText.substring(0, getRandomInt(40, reviewText.length))
-  });
+  const reviews = [];
+
+  for (let i = 0; i < num; i++) {
+    const review = new Review({
+      author: mongoose.Types.ObjectId("5c40ad7fe47e411f395a0cf7"),
+      course: mongoose.Types.ObjectId(courseId),
+      semester: getRandomSemester(),
+      year: getRandomYear(),
+      rating: getRandomInt(1, 5),
+      difficulty: getRandomInt(1, 5),
+      workload: getRandomInt(4, 40),
+      body: faker.lorem.paragraphs(getRandomInt(1, 3))
+    })
+
+    reviews.push(review);
+  }
+
+  return Review
+    .insertMany(reviews)
+    .then((res) => { 
+       return res;
+    })
+    .catch((err) => console.log(err));
 }
 
-console.log(JSON.stringify(data, null, 2));
+async function processCourses(courses) {
+  for (const course of courses) {
+    const ret = randomizeReviews(course.id);
+    await ret;
+  }
+};
+
+function main() {
+  mongoose
+    .connect(uri, { useNewUrlParser: true })
+    .then(() => {
+      return Course
+        .find({})
+        .then((allCourses) => {
+            return Promise.all(allCourses);
+        })
+        .catch((err) => { console.log(err) });
+    })
+    .then((courses) => {
+      return processCourses(courses);
+    })
+    .then(() => {
+      console.log('Done');
+      mongoose.disconnect();
+    })
+    .catch((err) => { console.log(err); });
+}
+
+main();
