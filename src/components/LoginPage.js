51feca3a4feaaea345094ca78b8fed34158c95e3
alexerhardt <alex.erhardt.es@gmail.com>
2019-02-27 19:46:49 +0100

    add redirect on successful login

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,26 +1,45 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Header from './Header';
+/* eslint-disable class-methods-use-this */
 import LoginForm from './LoginForm';
 
-const LoginPage = () => (
-  <div id="container-loginpage">
-    <Header />
-    <div className="container container-inner__authpage">
-      <div className="columns p-2 mt-4">
-        <div className="column col-6 col-sm-12 col-mx-auto">
-            <LoginForm
-              isOpen
-            />
-        </div>
-      </div>
-      <div className="columns p-2">
-        <div className="column col-6 col-sm-12 col-mx-auto mt-3 text-center">
-            <p>Don't have an account? Sign up <Link to="/signup">here.</Link></p>
-        </div>
-      </div>
-    </div>
-  </div>
-);
+class LoginPage extends React.Component {
+  componentDidUpdate() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/');
+    }
+  }
 
-export default LoginPage;
+  render() {
+    return (
+      <div id="container-loginpage">
+        <Header />
+        <div className="container container-inner__authpage">
+          <div className="columns p-2 mt-4">
+            <div className="column col-6 col-sm-12 col-mx-auto">
+                <LoginForm
+                  isOpen
+                />
+            </div>
+          </div>
+          <div className="columns p-2">
+            <div className="column col-6 col-sm-12 col-mx-auto mt-3 text-center">
+                <p>Don't have an account? Sign up <Link to="/signup">here.</Link></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    )
+  }
+}
+
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, {})(LoginPage);
+
